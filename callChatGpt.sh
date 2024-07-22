f5c89e5b873266c2148f185b45e0d68d7efae327
Ben Schilke <bschilke.biz@gmail.com>
2024-07-21 21:22:14 -0400

    controlled scrolling with less command

--- callChatGpt.sh
+++ callChatGpt.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo ""
+echo "output will be provided in less -X format, so scroll thorugh output using SPACE, b"
+echo "press q when done (output displayed will remain in the screen)"
+echo ""
+
 # Call the Python application with the arguments
-python src/callChatGpt.py "$*"
+python src/callChatGpt.py "$*" | less -X
 
 
